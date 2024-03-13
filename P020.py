54290ff883bb43a078309d8d8e7b3d2d7d66a720
Enrique Garcia Illera <egaillera@gmail.com>
2024-03-13 09:41:47 +0100

    Solution of problem 20

---
+++ P020.py
@@ -0,0 +1,21 @@
+'''
+Find the sum of the digits in the number 100!
+'''
+from time import time
+
+start = time()
+	
+fact = 1
+for i in range(1,101):
+	fact = fact*i
+	
+str_fact = str(fact)
+
+total= 0
+for i in range(len(str_fact)):
+	#print(str_fact[i])
+	total = total + int(str_fact[i])
+	
+
+elapsed = time() - start
+print("Solution: {0} \t Time elapsed: {1}".format(total, elapsed))
